7d895cea2266e3cd8d65794d0a12b8c513f7837c
Dariusz Szut <dew326@gmail.com>
2019-03-25 13:33:13 +0100

    EZP-30345: Add JS to webpack encore entry from bundle

---
+++ src/bundle/Resources/encore/ez.config.manager.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = (eZConfig, eZConfigManager) => {
+    eZConfigManager.add({
+        eZConfig,
+        entryName: 'ezplatform-admin-ui-content-type-edit-js',
+        newItems: [path.resolve(__dirname, '../public/js/scripts/admin.contenttype.matrix.js')],
+    });
+
+    eZConfigManager.add({
+        eZConfig,
+        entryName: 'ezplatform-admin-ui-content-edit-parts-js',
+        newItems: [path.resolve(__dirname, '../public/js/scripts/fieldType/ezmatrix.js')],
+    });
+};
