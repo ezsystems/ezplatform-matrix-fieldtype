d44965e8a64d80c42336167b87bd032eb69d6d0d
Nattfarinn <nattfarinn@gmail.com>
2019-03-29 14:17:35 +0100

    wip: Migration

--- src/bundle/Command/MigrateLegacyMatrixCommand.php
+++ src/bundle/Command/MigrateLegacyMatrixCommand.php
@@ -10,10 +10,12 @@ namespace EzSystems\EzPlatformMatrixFieldtypeBundle\Command;
 
 use Doctrine\DBAL\Connection;
 use eZ\Publish\Core\Persistence\Legacy\Content\StorageFieldDefinition;
+use eZ\Publish\Core\Persistence\Legacy\Content\StorageFieldValue;
+use eZ\Publish\SPI\Persistence\Content\FieldValue;
 use eZ\Publish\SPI\Persistence\Content\Type\FieldDefinition;
 use EzSystems\EzPlatformMatrixFieldtype\FieldType\Converter\MatrixConverter;
-use SimpleXMLElement;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -30,25 +32,13 @@ class MigrateLegacyMatrixCommand extends Command
     /** @var \Doctrine\DBAL\Connection */
     private $connection;
 
-    /** @var string */
-    private $contentclassAttributeSchema;
-
-    /** @var string */
-    private $contentobjectAttributeSchema;
-
     /**
      * @param \Doctrine\DBAL\Connection $connection
-     * @param string $contentclassAttributeSchema
-     * @param string $contentobjectAttributeSchema
      */
     public function __construct(
-        Connection $connection,
-        string $contentclassAttributeSchema,
-        string $contentobjectAttributeSchema
+        Connection $connection
     ) {
         $this->connection = $connection;
-        $this->contentclassAttributeSchema = $contentclassAttributeSchema;
-        $this->contentobjectAttributeSchema = $contentobjectAttributeSchema;
 
         parent::__construct();
     }
@@ -72,9 +62,147 @@ class MigrateLegacyMatrixCommand extends Command
     {
         $io = new SymfonyStyle($input, $output);
 
+        $io->comment('Migrating legacy ezmatrix fieldtype');
+
         $iterationCount = (int)$input->getOption('iteration-count');
         $converter = new MatrixConverter();
 
+        $contentClassAttributes = $this->getContentClassAttributes();
+
+        libxml_use_internal_errors(true);
+
+        foreach ($contentClassAttributes as $contentClassAttribute) {
+            $io->comment(sprintf('Migrate %s:%s attribute.', $contentClassAttribute['contenttype_identifier'], $contentClassAttribute['identifier']));
+
+            $xml = simplexml_load_string((string)$contentClassAttribute['columns']);
+
+            if ($xml) {
+                $columnList = $xml->xpath('//column-name');
+
+                $columns = [];
+
+                foreach ($columnList as $column) {
+                    $columns[(int)$column['idx']] = [
+                        'identifier' => (string)$column['id'],
+                        'name' => (string)$column,
+                    ];
+                }
+
+                $fieldDefinition = new FieldDefinition();
+                $storageFieldDefinition = new StorageFieldDefinition();
+
+                $fieldDefinition->fieldTypeConstraints->fieldSettings = [
+                    'minimum_rows' => $contentClassAttribute['minimum_rows'],
+                    'columns' => array_values($columns),
+                ];
+
+                $converter->toStorageFieldDefinition($fieldDefinition, $storageFieldDefinition);
+
+                $this->updateContentClassAttribute(
+                    (int)$contentClassAttribute['id'],
+                    (int)$storageFieldDefinition->dataInt1,
+                    (string)$storageFieldDefinition->dataText5
+                );
+
+                $columnsJson = $storageFieldDefinition->dataText5;
+            } else {
+                $columnsJson = $contentClassAttribute['columns'];
+
+                dump($columnsJson); die();
+            }
+
+            $contentAttributesCount = $this->getContentObjectAttributesCount(
+                (int)$contentClassAttribute['id']
+            );
+
+            $columns = json_decode($columnsJson);
+
+            $progressBar = $this->getProgressBar($contentAttributesCount, $output);
+            $progressBar->start();
+
+            for ($offset = 0; $offset <= $contentAttributesCount; $offset += $iterationCount) {
+                gc_disable();
+
+                $contentObjectAttributes = $this->getContentObjectAttributes(
+                    (int)$contentClassAttribute['id'],
+                    $offset,
+                    $iterationCount
+                );
+
+                foreach ($contentObjectAttributes as $contentObjectAttribute) {
+                    $xml = simplexml_load_string(
+                        (string)$contentObjectAttribute['data_text']
+                    );
+
+                    $storageFieldValue = new StorageFieldValue();
+                    $fieldValue = new FieldValue([
+                        'data' => [
+                            'entries' => [],
+                        ]
+                    ]);
+
+                    if (!$xml) {
+                        $progressBar->advance(1);
+
+                        continue;
+                    }
+
+                    $rows = $this->convertCellsToRows($xml->xpath('c'), $columns);
+
+                    $fieldValue->data['entries'] = $rows;
+
+                    $converter->toStorageValue($fieldValue, $storageFieldValue);
+
+                    $this->updateContentObjectAttribute(
+                        (int)$contentObjectAttribute['id'],
+                        (string)$storageFieldValue->dataText
+                    );
+
+                    $progressBar->advance(1);
+                }
+
+                gc_enable();
+            }
+
+            $progressBar->finish();
+
+            $output->writeln(['', '']);
+        }
+
+        $io->success('Done.');
+    }
+
+    /**
+     * @param array $cells
+     * @param array $columns
+     *
+     * @return array
+     */
+    private function convertCellsToRows(array $cells, array $columns): array
+    {
+        $rows = [];
+        $columnsCount = count($columns);
+
+        foreach ($cells as $index => $cell) {
+            $columnIndex = $index % $columnsCount;
+            $columnIdentifier = $columns[$columnIndex]->identifier;
+
+            $row[$columnIdentifier] = (string)$cell;
+
+            if ($columnIndex === $columnsCount - 1) {
+                $rows[] = $row;
+                $row = [];
+            }
+        }
+
+        return $rows;
+    }
+
+    /**
+     * @return array
+     */
+    private function getContentClassAttributes(): array
+    {
         $query = $this->connection->createQueryBuilder();
         $query
             ->select([
@@ -89,66 +217,97 @@ class MigrateLegacyMatrixCommand extends Command
             ->where('attr.data_type_string = :identifier')
             ->setParameter(':identifier', self::EZMATRIX_IDENTIFIER);
 
-        libxml_use_internal_errors(true);
+        return $query->execute()->fetchAll();
+    }
 
-        foreach ($query->execute()->fetchAll() as $attribute) {
-            $io->comment(sprintf('Migrate %s:%s attribute.', $attribute['contenttype_identifier'], $attribute['identifier']));
+    /**
+     * @param int $id
+     * @param int $minimumRows
+     * @param string $columns
+     */
+    private function updateContentClassAttribute(int $id, int $minimumRows, string $columns): void
+    {
+        $query = $this->connection->createQueryBuilder();
+        $query
+            ->update('ezcontentclass_attribute', 'attr')
+            ->set('attr.data_int1', ':minimum_rows')
+            ->set('attr.data_text5', ':columns')
+            ->where('attr.id = :id')
+            ->setParameter(':id', $id)
+            ->setParameter(':minimum_rows', $minimumRows)
+            ->setParameter(':columns', $columns);
 
-            $xml = simplexml_load_string($attribute['columns']);
+        $query->execute();
+    }
 
-            if (!$xml) {
-                $io->warning('Attribute definition is not an XML.');
-            }
+    /**
+     * @param int $id
+     *
+     * @return int
+     */
+    private function getContentObjectAttributesCount(int $id): int
+    {
+        $query = $this->connection->createQueryBuilder();
+        $query
+            ->select('count(1)')
+            ->from('ezcontentobject_attribute', 'attr')
+            ->where('attr.contentclassattribute_id = :class_attr_id')
+            ->setParameter(':class_attr_id', $id);
 
-            $columnList = $xml->xpath('//column-name');
+        return (int)$query->execute()->fetchColumn(0);
+    }
 
-            $columns = [];
+    /**
+     * @param int $id
+     * @param int $offset
+     * @param int $iterationCount
+     *
+     * @return array
+     */
+    private function getContentObjectAttributes(int $id, int $offset, int $iterationCount): array
+    {
+        $query = $this->connection->createQueryBuilder();
+        $query
+            ->select(['id', 'data_text'])
+            ->from('ezcontentobject_attribute', 'attr')
+            ->where('attr.contentclassattribute_id = :class_attr_id')
+            ->setParameter(':class_attr_id', $id)
+            ->setFirstResult($offset)
+            ->setMaxResults($iterationCount);
 
-            foreach ($columnList as $column) {
-                $columns[(int)$column['idx']] = [
-                    'identifier' => (string)$column['id'],
-                    'name' => (string)$column,
-                ];
-            }
+        return $query->execute()->fetchAll();
+    }
 
-            $fieldDefinition = new FieldDefinition();
-            $storageFieldDefinition = new StorageFieldDefinition();
+    /**
+     * @param int $id
+     * @param string $rows
+     */
+    private function updateContentObjectAttribute(int $id, string $rows): void
+    {
+        $query = $this->connection->createQueryBuilder();
+        $query
+            ->update('ezcontentobject_attribute', 'attr')
+            ->set('attr.data_text', ':rows')
+            ->where('attr.id = :id')
+            ->setParameter(':id', $id)
+            ->setParameter(':rows', $rows);
 
-            $fieldDefinition->fieldTypeConstraints->fieldSettings = [
-                'minimum_rows' => $attribute['minimum_rows'],
-                'columns' => array_values($columns),
-            ];
+        $query->execute();
+    }
 
-            $converter->toStorageFieldDefinition($fieldDefinition, $storageFieldDefinition);
-            dump($storageFieldDefinition);
+    /**
+     * @param int $maxSteps
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     *
+     * @return \Symfony\Component\Console\Helper\ProgressBar
+     */
+    protected function getProgressBar(int $maxSteps, OutputInterface $output): ProgressBar
+    {
+        $progressBar = new ProgressBar($output, $maxSteps);
+        $progressBar->setFormat(
+            ' %current%/%max% [%bar%] %percent:3s%% %elapsed:6s%/%estimated:-6s% %memory:6s%'
+        );
 
-            $query = $this->connection->createQueryBuilder();
-            $query
-                ->update('ezcontentclass_attribute', 'attr')
-                ->set('attr.data_int1', ':minimum_rows')
-                ->set('attr.data_text5', ':columns')
-                ->where('attr.id = :id')
-                ->setParameter(':id', $attribute['id'])
-                ->setParameter(':mu', $attribute['id']);
-
-            dump($query->getSQL()); die();
-
-
-            die();
-        }
-
-
-        /*
-        for ($offset = 0; $offset <= $locationsCount; $offset += $iterationCount) {
-            gc_disable();
-            $locations = $this->repository->sudo(
-                function (Repository $repository) use ($offset, $iterationCount) {
-                    return $repository->getLocationService()->loadAllLocations($offset, $iterationCount);
-                }
-            );
-            $this->processLocations($locations, $progressBar);
-            gc_enable();
-        }
-        */
+        return $progressBar;
     }
 }
