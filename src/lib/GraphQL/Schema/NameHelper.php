e835daa9cfa99a8cc38ad5b94dc55f6354f1a153
Bertrand Dunogier <bertrand.dunogier@ez.no>
2019-12-20 14:16:36 +0100

    GraphQL support (#20)
    * Implemented GraphQL support
    
    Each ezmatrix field definition gets its own GraphQL type, named after the content type and field definition identifiers:
    the "features" field definition of the "product" content type will use:
    - ProductFeatureRow for querying
    - ProductFeatureRowInput for mutations
    
    The row objects will contain the defined columns as properties.
    
    * fixup! Implemented GraphQL support
    
    * Handled empty matrix rows notices
    
    Changes the Row objects to Silent Row objects that ignore missing indexes.

---
+++ src/lib/GraphQL/Schema/NameHelper.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\EzPlatformMatrixFieldtype\GraphQL\Schema;
+
+use eZ\Publish\API\Repository\Values\ContentType\ContentType;
+use eZ\Publish\API\Repository\Values\ContentType\FieldDefinition;
+use Symfony\Component\Serializer\NameConverter\CamelCaseToSnakeCaseNameConverter;
+
+class NameHelper
+{
+    public function matrixFieldDefinitionType(ContentType $contentType, FieldDefinition $fieldDefinition)
+    {
+        $caseConverter = new CamelCaseToSnakeCaseNameConverter(null, false);
+
+        return sprintf(
+            '%s%sRow',
+            $caseConverter->denormalize($contentType->identifier),
+            $caseConverter->denormalize($fieldDefinition->identifier)
+        );
+    }
+
+    public function matrixFieldDefinitionInputType(ContentType $contentType, FieldDefinition $fieldDefinition)
+    {
+        $caseConverter = new CamelCaseToSnakeCaseNameConverter(null, false);
+
+        return sprintf(
+            '%s%sRowInput',
+            $caseConverter->denormalize($contentType->identifier),
+            $caseConverter->denormalize($fieldDefinition->identifier)
+        );
+    }
+}
