afe36cb129d79b7508167a921c33a963289c1bbc
Mikolaj Adamczyk <mikadamczyk@gmail.com>
2020-01-13 10:31:11 +0100

    Translation configuration name has been unified (#27)

--- src/bundle/DependencyInjection/EzPlatformMatrixFieldtypeExtension.php
+++ src/bundle/DependencyInjection/EzPlatformMatrixFieldtypeExtension.php
@@ -64,7 +64,7 @@ class EzPlatformMatrixFieldtypeExtension extends Extension implements PrependExt
     {
         $container->prependExtensionConfig('jms_translation', [
             'configs' => [
-                'matrix_fieldtype' => [
+                'ezplatform_matrix_fieldtype' => [
                     'dirs' => [
                         __DIR__ . '/../../',
                     ],
